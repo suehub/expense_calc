40b66f559cfa42aa038f4b1fd7d50e5a25f4a81a
suehub <dldustn2040@gmail.com>
2023-08-22 17:08:16 +0900

    Refactor: Function Component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,30 +1,30 @@
-import React, { Component } from "react";
+import React from "react";
 import "./ExpenseItem.css";
 import { MdEdit, MdDelete } from "react-icons/md";
 
-export default class ExpenseItem extends Component {
-  render() {
-    console.log(this.props.expense);
-    return (
-      <li className="item">
-        <div className="info">
-          <span className="expense">{this.props.expense.charge}</span>
-          <span className="amount">{this.props.expense.amount} 원</span>
-        </div>
-        <div>
-          <button className="edit-btn">
-            <MdEdit />
-          </button>
-          <button
-            className="clear-btn"
-            onClick={() => {
-              this.props.handleDelete(this.props.expense.id);
-            }}
-          >
-            <MdDelete />
-          </button>
-        </div>
-      </li>
-    );
-  }
-}
+const ExpenseItem = ({ expense, handleDelete }) => {
+  console.log(expense);
+  return (
+    <li className="item">
+      <div className="info">
+        <span className="expense">{expense.charge}</span>
+        <span className="amount">{expense.amount} 원</span>
+      </div>
+      <div>
+        <button className="edit-btn">
+          <MdEdit />
+        </button>
+        <button
+          className="clear-btn"
+          onClick={() => {
+            handleDelete(expense.id);
+          }}
+        >
+          <MdDelete />
+        </button>
+      </div>
+    </li>
+  );
+};
+
+export default ExpenseItem;
--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,28 +1,29 @@
-import React, { Component } from "react";
+import React from "react";
 import ExpenseItem from "./ExpenseItem.js";
 import "./ExpenseList.css";
 import { MdDelete } from "react-icons/md";
 
-export default class ExpenseList extends Component {
-  render() {
-    return (
-      <>
-        <ul className="list">
-          {this.props.expenses.map((expense) => {
-            return (
-              <ExpenseItem
-                key={expense.id}
-                expense={expense}
-                handleDelete={this.props.handleDelete}
-              />
-            );
-          })}
-        </ul>
-        <button className="btn">
-          목록 지우기
-          <MdDelete />
-        </button>
-      </>
-    );
-  }
-}
+const ExpenseList = ({ expenses, handleDelete }) => {
+  // console.log(expenses);
+  return (
+    <>
+      <ul className="list">
+        {expenses.map((expense) => {
+          return (
+            <ExpenseItem
+              key={expense.id}
+              expense={expense}
+              handleDelete={handleDelete}
+            />
+          );
+        })}
+      </ul>
+      <button className="btn">
+        목록 지우기
+        <MdDelete />
+      </button>
+    </>
+  );
+};
+
+export default ExpenseList;
